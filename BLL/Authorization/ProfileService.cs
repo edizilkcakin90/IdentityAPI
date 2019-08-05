86601a861249d5cdb8e230991320e7ad53d26a2a
edizilkcakin90 <ediz.ilkcakin@gmail.com>
2019-08-05 11:02:47 +0300

    authorization.

---
+++ BLL/Authorization/ProfileService.cs
@@ -0,0 +1,54 @@
+﻿using Core;
+using DAL;
+using IdentityServer4.Extensions;
+using IdentityServer4.Models;
+using IdentityServer4.Services;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Authorization
+{
+    public class ProfileService : IProfileService
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly IUserClaimsPrincipalFactory<User> _claimsFactory;
+
+        public ProfileService(UserManager<User> userManager, IUserClaimsPrincipalFactory<User> claimsFactory)
+        {
+            _userManager = userManager;
+            _claimsFactory = claimsFactory;
+        }
+
+        public async Task GetProfileDataAsync(ProfileDataRequestContext context)
+        {
+            var sub = context.Subject.GetSubjectId();
+            var user = await _userManager.FindByIdAsync(sub);
+            var principal = await _claimsFactory.CreateAsync(user);
+
+            var claims = principal.Claims.ToList();
+            claims = claims.Where(claim => context.RequestedClaimTypes.Contains(claim.Type)).ToList();
+
+            if (user.Email != null)
+                claims.Add(new Claim(PropertyConstants.Email, user.Email));
+
+            if (user.Name != null)
+                claims.Add(new Claim(PropertyConstants.Name, user.Name));
+
+            context.IssuedClaims = claims;
+        }
+
+
+        public async Task IsActiveAsync(IsActiveContext context)
+        {
+            var sub = context.Subject.GetSubjectId();
+            var user = await _userManager.FindByIdAsync(sub);
+
+            context.IsActive = (user != null) && user.IsEnabled;
+        }
+    }
+}
---
+++ Core/Helpers/Utilities.cs
@@ -0,0 +1,16 @@
+﻿using IdentityModel;
+using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Text;
+
+namespace Core.Helpers
+{
+    public class Utilities
+    {
+        public static string GetUserId(ClaimsPrincipal user)
+        {
+            return user.FindFirst(JwtClaimTypes.Subject)?.Value?.Trim();
+        }
+    }
+}
